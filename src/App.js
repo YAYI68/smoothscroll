c592dd5bcd4f019504b8a4723a914631c32a7540
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2022-09-19 06:48:29 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+   const [ isVisible, setIsVisible] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+      <h1 className="header">
+        <p>{isVisible?'not in the viewport': 'in the viewport'}</p>
+      </h1>
+      <div className='gap'></div>
+        <img src="./images/attraction.jpg" alt="simpsons" />  
+     </>
   );
 }
 
